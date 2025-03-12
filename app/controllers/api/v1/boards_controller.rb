e167b0da752c8707adcbd529f9c02c139d3dd4f7
denialtorres <denial.torres@gmail.com>
2025-03-11 22:02:34 -0600

    post board endpoint

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -1,6 +1,9 @@
 module Api
   module V1
     class BoardsController < ApplicationController
+      # skip rails auth tokens
+      # fix for ActionController::InvalidAuthenticityToken (Can't verify CSRF token authenticity.):
+      skip_before_action :verify_authenticity_token, railse: false
       before_action :authenticate_devise_api_token!
       before_action :set_current_user
       before_action :set_board, only: [:show]
@@ -16,6 +19,17 @@ module Api
         render json: BoardSerializer.new(@board).serializable_hash.to_json
       end
 
+      # POST /api/v1/boards
+      def create
+        board = current_user.boards.new(board_params)
+
+        if board.save
+          render json: BoardSerializer.new(board).serializable_hash.to_json, status: :created
+        else
+          render json: { error: board.errors.messages }, status: :unprocessable_entity
+        end
+      end
+
       private
 
       def set_current_user
@@ -27,6 +41,10 @@ module Api
         @board = current_user.boards.find(params[:id])
       rescue ActiveRecord::RecordNotFound
         render json: { error: "Board not found" }, status: :not_found
+      end
+
+      def board_params
+        params.require(:board).permit(:name)
       end
     end
   end
---
+++ spec/requests/api/v1/post_board_spec.rb
@@ -0,0 +1,60 @@
+require "rails_helper"
+require "swagger_helper"
+
+RSpec.describe "POST /api/v1/boards", type: :request do
+  path "/api/v1/boards" do
+    post "Generates a new authenticated user's board" do
+      tags "Boards"
+      consumes "application/json"
+      produces "application/json"
+
+      parameter name: :Authorization, in: :header, type: :string, required: true, description: "Bearer token"
+      parameter name: :board, in: :body, require: true, description: "Board main data", schema: {
+        type: :object,
+        properties: {
+          name: { type: :string }
+        },
+        required: ["name"]
+      }
+
+      response "201", "Board created" do
+        let(:user) { create(:user) }
+        let(:devise_api_token) { create(:devise_api_token, resource_owner: user) }
+        let(:board) { { name: "test name" } }
+
+        let(:Authorization) { "Bearer #{devise_api_token.access_token}" }
+
+        run_test! do
+          expect(response).to have_http_status(:success)
+          body = JSON.parse(response.body)
+          expect(body["data"]["attributes"]["name"]).to eq(board[:name])
+        end
+      end
+
+      response "401", "Invalid token - unauthorized" do
+        let(:Authorization) { "Bearer invalid_or_expired_token" }
+        let(:board) { { name: "test name" } }
+
+        run_test! do
+          expect(response).to have_http_status(:unauthorized)
+          body = JSON.parse(response.body)
+          expect(body["error"]).to be_present
+        end
+      end
+
+      response "422", "Missing params" do
+        let(:user) { create(:user) }
+        let(:devise_api_token) { create(:devise_api_token, resource_owner: user) }
+        let(:board) { { name: "" } }
+
+        let(:Authorization) { "Bearer #{devise_api_token.access_token}" }
+
+        run_test! do
+          expect(response).to have_http_status(:unprocessable_entity)
+          body = JSON.parse(response.body)
+          expect(body["error"]["name"]).to eq(["can't be blank"])
+        end
+      end
+    end
+  end
+end
