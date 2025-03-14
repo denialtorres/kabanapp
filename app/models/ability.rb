6d246956d1ef2cace9faf10dc07557236ae1b07f
denialtorres <denial.torres@gmail.com>
2025-03-13 20:46:23 -0600

    refactor roles class

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,27 +4,41 @@ class Ability
   def initialize(user)
     return unless user.present?
 
-    if user.super_admin?
-      can :manage, :all
-    else
-      # anyone can see the boards and the cards
-      can :read, Board
-      can :read, Card
+    case user.role
+    when 'super_admin'
+      super_admin_abilities
+    when 'owner'
+      owner_abilities(user)
+    when 'user'
+      user_abilities(user)
     end
 
-    # Owners can manage cards aslong they are the creators of the board
-    if user.role == "owner"
-      can [ :create, :update, :assign, :move, :unassign ], Card, column: { board: { user_id: user.id } }
+    public_abilities
+  end
 
-      can [ :create, :update ], Board
+  private
 
-      # board authors and super admins can delete boards
-      can :destroy, Board, user_id: user.id
-    end
+  def super_admin_abilities
+    can :manage, :all
+  end
 
-    if user.role == "user"
-      # Regular users can only move cards they are assigned to
-      can :move, Card, user_cards: { user_id: user.id }
-    end
+  def owner_abilities(user)
+    # owners can manage cards as long as they are the creators of the board
+    can [:create, :update, :assign, :move, :unassign], Card, column: { board: { user_id: user.id } }
+
+    can [:create, :update], Board
+    # board authors and super admins can delete boards
+    can :destroy, Board, user_id: user.id
+  end
+
+  def user_abilities(user)
+    # regular users can only move cards they are assigned to
+    can :move, Card, user_cards: { user_id: user.id }
+  end
+
+  def public_abilities
+    # anyone can see the boards and the cards
+    can :read, Board
+    can :read, Card
   end
 end
