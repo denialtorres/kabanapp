6d083083bd90a9e5503ffe06c7232b0df96bfe20
denialtorres <denial.torres@gmail.com>
2025-03-16 22:02:04 -0600

    rotulus

--- config/initializers/rotulus.rb
+++ config/initializers/rotulus.rb
@@ -1,5 +1,5 @@
 Rotulus.configure do |config|
   config.page_default_limit = 10
   config.page_max_limit = 10
-  config.secret = ENV["ROTULUS_SECRET"]
+  config.secret = ENV.fetch("ROTULUS_SECRET", "secret")
 end
