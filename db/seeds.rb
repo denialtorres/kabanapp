2904abc9b4eb4913417c3ffc314d690e3528822e
denialtorres <denial.torres@gmail.com>
2025-03-18 11:07:34 -0600

    include seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+UserCard.destroy_all
+Board.destroy_all
+
+User.find_or_create_by!(email: "admin@mail.com") do |user|
+  user.password = "password123"
+  user.password_confirmation = "password123"
+  user.role = "super_admin"
+end
+
+User.find_or_create_by!(email: "owner@mail.com") do |user|
+  user.password = "password123"
+  user.password_confirmation = "password123"
+  user.role = "owner"
+end
