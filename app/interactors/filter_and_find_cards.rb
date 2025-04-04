d49e9359f9eb05e2555578545ddb7f44347da99d
denialtorres <denial.torres@gmail.com>
2025-04-03 19:39:29 -0600

    replace FilterCards and FindCards to only FilterAndFindCards

---
+++ app/interactors/filter_and_find_cards.rb
@@ -0,0 +1,34 @@
+class FilterAndFindCards
+  include Interactor
+
+  def call
+    context.cards = user.cards.eager_load(:column).ransack(serach_params).result
+  end
+
+  private
+
+  delegate :user, :params, to: :context
+
+  # ("columns"."position" = 2 AND (("cards"."name" ILIKE '%3427%') OR ("cards"."description" ILIKE '%3427%')))
+  def serach_params
+    {
+      g: [
+        {
+          m: "or",
+          name_cont_any: params[:query].presence,
+          description_cont_any: params[:query].presence,
+        }
+      ],
+      column_position_eq:status_index[params[:status]].presence,
+    }.compact
+  end
+
+
+  def status_index
+    {
+      "to_do" => 0,
+      "in_progress" => 1,
+      "done" => 2
+    }
+  end
+end
