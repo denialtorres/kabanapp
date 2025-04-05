2cf7995a5de0b84e78d4d52ff7dacb804c399b0a
denialtorres <denial.torres@gmail.com>
2025-04-04 22:13:00 -0600

    add query descriptions

--- app/graphql/boards/queries.rb
+++ app/graphql/boards/queries.rb
@@ -3,9 +3,13 @@ module Boards
     extend ActiveSupport::Concern
 
     included do
-      field :boards, [ Types::BoardType ], null: false
-      field :board, Types::BoardType, null: false do
-        argument :id, GraphQL::Types::ID, required: true
+      field :boards, [ Types::BoardType ], null: false,
+        description: "Retrieve a list of all boards that belong to the current user."
+
+      field :board, Types::BoardType, null: false,
+        description: "Fetch a specific board by its ID, belonging to the current user." do
+          argument :id, GraphQL::Types::ID, required: true,
+            description: "The unique identifier of the board."
       end
     end
 
