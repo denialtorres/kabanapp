a3f9f20083a250607e95dc970e5e627b10a90b1e
denialtorres <denial.torres@gmail.com>
2025-04-05 00:00:05 -0600

    graphiql fix

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,8 @@
 //= link_tree ../images
 //= link fastentry/application.css
 //= link fastentry/application.js
+//= link graphiql/rails/graphiql-3.8.3.css
+//= link graphiql/rails/application.js
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
