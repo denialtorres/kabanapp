c78e32e6d9da4b49c3e46cec18d93251f3617aed
denialtorres <denial.torres@gmail.com>
2025-04-06 11:48:01 -0600

    swagger config

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,14 +4,14 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-Rails.application.configure do
-  config.content_security_policy do |policy|
-    policy.default_src :self
-    policy.connect_src :self, "https://demoapp-daniel.fun"
-    # Specify URI for violation reports
-    # policy.report_uri "/csp-violation-report-endpoint"
-  end
-end
+# Rails.application.configure do
+#   config.content_security_policy do |policy|
+#     policy.default_src :self
+#     policy.connect_src :self, "https://demoapp-daniel.fun"
+#     # Specify URI for violation reports
+#     # policy.report_uri "/csp-violation-report-endpoint"
+#   end
+# end
 #   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
 #   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
 #   config.content_security_policy_nonce_directives = %w(script-src style-src)
