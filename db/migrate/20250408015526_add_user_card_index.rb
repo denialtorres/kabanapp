a2f5f2ace544b348f889d932b477b59f299eca7c
denialtorres <denial.torres@gmail.com>
2025-04-07 20:00:40 -0600

    rubocop

--- db/migrate/20250408015526_add_user_card_index.rb
+++ db/migrate/20250408015526_add_user_card_index.rb
@@ -1,5 +1,5 @@
 class AddUserCardIndex < ActiveRecord::Migration[8.0]
   def change
-    add_index :user_cards, [:user_id, :card_id], unique: true, name: "index_user_cards_on_user_and_card"
+    add_index :user_cards, [ :user_id, :card_id ], unique: true, name: "index_user_cards_on_user_and_card"
   end
 end
